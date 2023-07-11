4db06d762c842d60dbb8ff433cb9ed9fd186efd4
jorge niglia <jorge.niglia@gmail.com>
2023-07-11 10:49:15 -0300

    instrospection true

--- src/index.js
+++ src/index.js
@@ -1,27 +1,25 @@
 const { ApolloServer } = require("@apollo/server");
-const {
-	startStandaloneServer,
-} = require("@apollo/server/standalone");
+const { startStandaloneServer } = require("@apollo/server/standalone");
 const typeDefs = require("./schema");
 const resolvers = require("./resolvers");
 const PetgramAPI = require("./datasources/petgram-api");
 
 async function starApolloServer() {
-	const server = new ApolloServer({ typeDefs, resolvers });
-	const { url } = await startStandaloneServer(server, {
-		context: async () => {
-			const { cache } = server;
-			return {
-				dataSources: {
-					petgramAPI: new PetgramAPI({ cache }),
-				},
-			};
-		},
-		listen: {
-			port: process.env.PORT || 4000,
-		},
-	});
-	console.log(`
+    const server = new ApolloServer({ typeDefs, resolvers, introspection: true });
+    const { url } = await startStandaloneServer(server, {
+        context: async () => {
+            const { cache } = server;
+            return {
+                dataSources: {
+                    petgramAPI: new PetgramAPI({ cache }),
+                },
+            };
+        },
+        listen: {
+            port: process.env.PORT || 4000,
+        },
+    });
+    console.log(`
     🚀  Server is running!
     📭  Query at ${url}
   `);
